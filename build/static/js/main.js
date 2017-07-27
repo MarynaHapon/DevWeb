07dbc86fd222a7f388cc44f31e940d181d3e310c
Maryna Hapon <marynahapon@gmail.com>
2017-07-27 12:47:42 +0300

    flickity
    carousel

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,4 +1,29 @@
 $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
+   $('#overlay-hidden').toggleClass('hidden-wrap') // .hidden-wrap --> common_styles.styl
   });
+
+// external js: flickity.pkgd.js
+var $carousel = $('.carousel').flickity({
+    autoPlay: 3000,
+    imagesLoaded: true,
+    percentPosition: false,
+});
+
+var $imgs = $carousel.find('.carousel-cell img');
+// get transform property
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ?
+    'transform' : 'WebkitTransform';
+// get Flickity instance
+var flkty = $carousel.data('flickity');
+
+$carousel.on( 'scroll.flickity', function() {
+    flkty.slides.forEach( function( slide, i ) {
+        var img = $imgs[i];
+        var x = ( slide.target + flkty.x ) * -1/3;
+        img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+    });
+});
+
